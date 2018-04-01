e447a7fb621f4f96d3cc2c255fc3d7fc69a3c1a0
SuhaasGupta <37362364+SuhaasGupta@users.noreply.github.com>
2018-04-01 21:44:58 +0530

    two files added

---
+++ cake.py
@@ -0,0 +1,15 @@
+
+for i in range(0,5):
+    print("What is your favorite food")
+    p = raw_input("")
+    if p == "cake":
+        print("your choice matches with 30% of the people")
+    elif p == "pizza":
+        print("you choice matches with 50% of the people")
+    elif p == "chips":
+        print("you choice matches with 10% of the people")
+    else:
+        print("your choice is not a popular one")
+    
+     
+
---
+++ table.py
@@ -0,0 +1,9 @@
+print("Enter the digit to know its table")
+t = int(raw_input(""))
+print("Enter the ending point of the table you need")
+a = int(raw_input(""))
+k = a+1
+for i in range(0,k):
+    print t,"*",i,"=",i*t
+
+    
